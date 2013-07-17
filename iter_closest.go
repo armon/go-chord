5986277246b7505325e22538d9e9f3fda1af6d58
Armon Dadgar <armon.dadgar@gmail.com>
2013-07-17 15:53:39 -0700

    Working on iterator, adding tests

--- iter_closest.go
+++ iter_closest.go
@@ -9,13 +9,15 @@ type ClosestPreceedingVnodeIterator struct {
 	vn            *localVnode
 	finger_idx    int
 	successor_idx int
+	yielded       map[string]struct{}
 }
 
 func (cp *ClosestPreceedingVnodeIterator) init(vn *localVnode, key []byte) {
 	cp.key = key
 	cp.vn = vn
-	cp.finger_idx = len(vn.successors)
-	cp.successor_idx = len(vn.finger)
+	cp.successor_idx = len(vn.successors) - 1
+	cp.finger_idx = len(vn.finger) - 1
+	cp.yielded = make(map[string]struct{})
 }
 
 func (cp *ClosestPreceedingVnodeIterator) Next() (*Vnode, error) {
@@ -30,6 +32,9 @@ func (cp *ClosestPreceedingVnodeIterator) Next() (*Vnode, error) {
 		if vn.successors[i] == nil {
 			continue
 		}
+		if _, ok := cp.yielded[vn.successors[i].String()]; ok {
+			continue
+		}
 		if between(vn.Id, cp.key, vn.successors[i].Id) {
 			successor_node = vn.successors[i]
 			break
@@ -40,6 +45,9 @@ func (cp *ClosestPreceedingVnodeIterator) Next() (*Vnode, error) {
 	// Scan to find the next finger
 	for i = cp.finger_idx; i >= 0; i-- {
 		if vn.finger[i] == nil {
+			continue
+		}
+		if _, ok := cp.yielded[vn.finger[i].String()]; ok {
 			continue
 		}
 		if between(vn.Id, cp.key, vn.finger[i].Id) {
@@ -53,28 +61,32 @@ func (cp *ClosestPreceedingVnodeIterator) Next() (*Vnode, error) {
 	if successor_node != nil && finger_node != nil {
 		// Determine the closer node
 		hb := cp.vn.ring.config.HashBits
-		closest := closest_vnode(successor_node, finger_node, cp.key, hb)
+		closest := closest_preceeding_vnode(successor_node,
+			finger_node, cp.key, hb)
 		if closest == successor_node {
 			cp.successor_idx--
 		} else {
 			cp.finger_idx--
 		}
+		cp.yielded[closest.String()] = struct{}{}
 		return closest, nil
 
 	} else if successor_node != nil {
 		cp.successor_idx--
+		cp.yielded[successor_node.String()] = struct{}{}
 		return successor_node, nil
 
 	} else if finger_node != nil {
 		cp.finger_idx--
+		cp.yielded[finger_node.String()] = struct{}{}
 		return finger_node, nil
 	}
 
 	return nil, nil
 }
 
-// Returns the closest Vnode to the key
-func closest_vnode(a, b *Vnode, key []byte, bits int) *Vnode {
+// Returns the closest preceeding Vnode to the key
+func closest_preceeding_vnode(a, b *Vnode, key []byte, bits int) *Vnode {
 	a_dist := distance(a.Id, key, bits)
 	b_dist := distance(b.Id, key, bits)
 	if a_dist.Cmp(b_dist) <= 0 {
@@ -84,7 +96,7 @@ func closest_vnode(a, b *Vnode, key []byte, bits int) *Vnode {
 	}
 }
 
-// Computes the minimum distance between two keys modulus a ring size
+// Computes the forward distance from a to b modulus a ring size
 func distance(a, b []byte, bits int) *big.Int {
 	// Get the ring size
 	var ring big.Int
@@ -96,18 +108,10 @@ func distance(a, b []byte, bits int) *big.Int {
 	(&b_int).SetBytes(b)
 
 	// Compute the distances
-	var dist_1, dist_2 big.Int
-	(&dist_1).Sub(&a_int, &b_int)
-	(&dist_2).Sub(&b_int, &a_int)
+	var dist big.Int
+	(&dist).Sub(&b_int, &a_int)
 
 	// Distance modulus ring size
-	(&dist_1).Mod(&dist_1, &ring)
-	(&dist_2).Mod(&dist_2, &ring)
-
-	// Take the smaller distance
-	if (&dist_1).Cmp(&dist_2) <= 0 {
-		return &dist_1
-	} else {
-		return &dist_2
-	}
+	(&dist).Mod(&dist, &ring)
+	return &dist
 }
--- iter_closest_test.go
+++ iter_closest_test.go
@@ -5,6 +5,61 @@ import (
 	"testing"
 )
 
+func TestNextClosest(t *testing.T) {
+	// Make the vnodes on the ring (mod 64)
+	v1 := &Vnode{Id: []byte{1}}
+	v2 := &Vnode{Id: []byte{10}}
+	//v3 := &Vnode{Id: []byte{20}}
+	v4 := &Vnode{Id: []byte{32}}
+	//v5 := &Vnode{Id: []byte{40}}
+	//v6 := &Vnode{Id: []byte{48}}
+	v7 := &Vnode{Id: []byte{62}}
+
+	// Make a vnode
+	vn := &localVnode{}
+	vn.Id = []byte{54}
+	vn.successors = []*Vnode{v7}
+	vn.finger = []*Vnode{nil, nil, nil, v1, v2, v4}
+	vn.ring = &Ring{}
+	vn.ring.config = &Config{HashBits: 6}
+
+	// Make an iterator
+	k := []byte{32}
+	cp := &ClosestPreceedingVnodeIterator{}
+	cp.init(vn, k)
+
+	// Iterate until we are done
+	s1, err := cp.Next()
+	if s1 != v2 || err != nil {
+		t.Fatalf("Expect v2. %v", s1)
+	}
+
+	s2, err := cp.Next()
+	if s2 != v1 || err != nil {
+		t.Fatalf("Expect v1. %v", s2)
+	}
+
+	s3, err := cp.Next()
+	if s3 != v7 || err != nil {
+		t.Fatalf("Expect v7. %v", s3)
+	}
+
+	s4, err := cp.Next()
+	if s4 != nil || err != nil {
+		t.Fatalf("Expect nil. %v", s4)
+	}
+}
+
+func TestClosest(t *testing.T) {
+	a := &Vnode{Id: []byte{128}}
+	b := &Vnode{Id: []byte{32}}
+	k := []byte{45}
+	c := closest_preceeding_vnode(a, b, k, 8)
+	if c != b {
+		t.Fatalf("expect b to be closer!")
+	}
+}
+
 func TestDistance(t *testing.T) {
 	a := []byte{63}
 	b := []byte{3}
@@ -16,14 +71,14 @@ func TestDistance(t *testing.T) {
 	a = []byte{0}
 	b = []byte{65}
 	d = distance(a, b, 7) // Ring size of 128
-	if d.Cmp(big.NewInt(63)) != 0 {
-		t.Fatalf("expect distance 63! %v", d)
+	if d.Cmp(big.NewInt(65)) != 0 {
+		t.Fatalf("expect distance 65! %v", d)
 	}
 
 	a = []byte{1}
 	b = []byte{255}
 	d = distance(a, b, 8) // Ring size of 256
-	if d.Cmp(big.NewInt(2)) != 0 {
-		t.Fatalf("expect distance 2! %v", d)
+	if d.Cmp(big.NewInt(254)) != 0 {
+		t.Fatalf("expect distance 254! %v", d)
 	}
 }
