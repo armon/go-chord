1f75fd20311ad557209187105464f16332f9213b
Armon Dadgar <armon.dadgar@gmail.com>
2013-07-18 12:29:01 -0700

    Increasing test coverage

--- iter_closest_test.go
+++ iter_closest_test.go
@@ -12,14 +12,14 @@ func TestNextClosest(t *testing.T) {
 	//v3 := &Vnode{Id: []byte{20}}
 	v4 := &Vnode{Id: []byte{32}}
 	//v5 := &Vnode{Id: []byte{40}}
-	//v6 := &Vnode{Id: []byte{48}}
+	v6 := &Vnode{Id: []byte{59}}
 	v7 := &Vnode{Id: []byte{62}}
 
 	// Make a vnode
 	vn := &localVnode{}
 	vn.Id = []byte{54}
-	vn.successors = []*Vnode{v7}
-	vn.finger = []*Vnode{nil, nil, nil, v1, v2, v4}
+	vn.successors = []*Vnode{v6, v7, nil}
+	vn.finger = []*Vnode{v6, v6, v7, v1, v2, v4, nil}
 	vn.ring = &Ring{}
 	vn.ring.config = &Config{HashBits: 6}
 
@@ -45,8 +45,103 @@ func TestNextClosest(t *testing.T) {
 	}
 
 	s4, err := cp.Next()
-	if s4 != nil || err != nil {
-		t.Fatalf("Expect nil. %v", s4)
+	if s4 != v6 || err != nil {
+		t.Fatalf("Expect v6. %v", s4)
+	}
+
+	s5, err := cp.Next()
+	if s5 != nil || err != nil {
+		t.Fatalf("Expect nil. %v", s5)
+	}
+}
+
+func TestNextClosestNoSucc(t *testing.T) {
+	// Make the vnodes on the ring (mod 64)
+	v1 := &Vnode{Id: []byte{1}}
+	v2 := &Vnode{Id: []byte{10}}
+	//v3 := &Vnode{Id: []byte{20}}
+	v4 := &Vnode{Id: []byte{32}}
+	//v5 := &Vnode{Id: []byte{40}}
+	v6 := &Vnode{Id: []byte{59}}
+	v7 := &Vnode{Id: []byte{62}}
+
+	// Make a vnode
+	vn := &localVnode{}
+	vn.Id = []byte{54}
+	vn.successors = []*Vnode{nil}
+	vn.finger = []*Vnode{v6, v6, v7, v1, v2, v4, nil}
+	vn.ring = &Ring{}
+	vn.ring.config = &Config{HashBits: 6}
+
+	// Make an iterator
+	k := []byte{32}
+	cp := &ClosestPreceedingVnodeIterator{}
+	cp.init(vn, k)
+
+	// Iterate until we are done
+	s1, err := cp.Next()
+	if s1 != v2 || err != nil {
+		t.Fatalf("Expect v2. %v", s1)
+	}
+
+	s2, err := cp.Next()
+	if s2 != v1 || err != nil {
+		t.Fatalf("Expect v1. %v", s2)
+	}
+
+	s3, err := cp.Next()
+	if s3 != v7 || err != nil {
+		t.Fatalf("Expect v7. %v", s3)
+	}
+
+	s4, err := cp.Next()
+	if s4 != v6 || err != nil {
+		t.Fatalf("Expect v6. %v", s4)
+	}
+
+	s5, err := cp.Next()
+	if s5 != nil || err != nil {
+		t.Fatalf("Expect nil. %v", s5)
+	}
+}
+
+func TestNextClosestNoFinger(t *testing.T) {
+	// Make the vnodes on the ring (mod 64)
+	//v1 := &Vnode{Id: []byte{1}}
+	//v2 := &Vnode{Id: []byte{10}}
+	//v3 := &Vnode{Id: []byte{20}}
+	//v4 := &Vnode{Id: []byte{32}}
+	//v5 := &Vnode{Id: []byte{40}}
+	v6 := &Vnode{Id: []byte{59}}
+	v7 := &Vnode{Id: []byte{62}}
+
+	// Make a vnode
+	vn := &localVnode{}
+	vn.Id = []byte{54}
+	vn.successors = []*Vnode{v6, v7, v7, nil}
+	vn.finger = []*Vnode{nil, nil, nil}
+	vn.ring = &Ring{}
+	vn.ring.config = &Config{HashBits: 6}
+
+	// Make an iterator
+	k := []byte{32}
+	cp := &ClosestPreceedingVnodeIterator{}
+	cp.init(vn, k)
+
+	// Iterate until we are done
+	s3, err := cp.Next()
+	if s3 != v7 || err != nil {
+		t.Fatalf("Expect v7. %v", s3)
+	}
+
+	s4, err := cp.Next()
+	if s4 != v6 || err != nil {
+		t.Fatalf("Expect v6. %v", s4)
+	}
+
+	s5, err := cp.Next()
+	if s5 != nil || err != nil {
+		t.Fatalf("Expect nil. %v", s5)
 	}
 }
 
@@ -55,6 +150,10 @@ func TestClosest(t *testing.T) {
 	b := &Vnode{Id: []byte{32}}
 	k := []byte{45}
 	c := closest_preceeding_vnode(a, b, k, 8)
+	if c != b {
+		t.Fatalf("expect b to be closer!")
+	}
+	c = closest_preceeding_vnode(b, a, k, 8)
 	if c != b {
 		t.Fatalf("expect b to be closer!")
 	}
